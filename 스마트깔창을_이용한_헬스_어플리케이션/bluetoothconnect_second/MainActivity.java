bbd349699c72585485b7ccaa570cf9300e02e7f1
boom02071 <boom02071@gmail.com>
2020-09-03 19:38:26 +0900

    스마트깔창을 이용한 헬스 어플리케이션
    안드로이드 어플리케이션 & 아두이노 & JAVA & 졸업 작품

---
+++ 스마트깔창을_이용한_헬스_어플리케이션/bluetoothconnect_second/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.bluetoothconnect_second;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Bluetooth.class);
+                startActivity(i);
+            }
+        });
+    }
+}
